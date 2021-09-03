0b9acb35c32708ddec98c8c251e391af315d775e
WKD622 <kuba.ziarko@gmail.com>
2021-09-03 11:39:29 +0200

    adds drawer

--- src/components/atoms/icons/EuroIcon/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EuroIconMaterialUi from '@material-ui/icons/Euro';
-
-const EuroIcon = (props) => (
-    <EuroIconMaterialUi {...props} />
-);
-
-export default EuroIcon;
